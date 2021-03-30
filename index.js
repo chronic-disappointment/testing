3f087dcf5a3be7172573d39722912cbbf13e95c0
Skoupidi ths koinwnias <skoupidi@github.com>
2021-03-30 21:53:35 +0300

    http

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("good bye world");
-});
+// app.get("/", (req, res) => {
+//   res.send("good bye world");
+// });
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`listening on port ${port}`);
+// });
+
+var http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("goodbye world");
+    res.end();
+  })
+  .listen(port);
