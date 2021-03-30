909359066f7454fc12fcfcf0fb3e836e8bbfd7cb
Skoupidi ths koinwnias <skoupidi@github.com>
2021-03-30 21:32:44 +0300

    update render

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("good bye world");
 });
 
 app.listen(port, () => {
