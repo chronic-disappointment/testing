baf5580991fa7a81497a3da9f4e4eb45cd44473d
Skoupidi ths koinwnias <skoupidi@github.com>
2021-03-31 01:18:23 +0300

    express upgrade

--- index.js
+++ index.js
@@ -10,12 +10,12 @@
 //   console.log(`listening on port ${port}`);
 // });
 
-var http = require("http");
+// var http = require("http");
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    // res.write("goodbye world");
-    res.end("goodbye world i am going to try it with express later on");
-  })
-  .listen(process.env.PORT);
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     // res.write("goodbye world");
+//     res.end("goodbye world i am going to try it with express later on");
+//   })
+//   .listen(process.env.PORT);
