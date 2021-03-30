3c03d5003dc50e850ef000b053a2aeed6735cff9
Skoupidi ths koinwnias <skoupidi@github.com>
2021-03-31 02:10:57 +0300

    sth

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("good bye world");
+  res.send("good bye world good bye people");
 });
 
 app.get("/satan", (req, res) => {
